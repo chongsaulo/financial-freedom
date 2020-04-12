e9c74a0033fb05162f9e22ebe8f932f313500fad
Anthony Lee <hososlch@gmail.com>
2020-04-12 16:15:15 +0800

    smaller view

--- src/app/components/Returns/components/ReturnCard.tsx
+++ src/app/components/Returns/components/ReturnCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Grid, Card, CardContent, Typography, makeStyles } from '@material-ui/core';
-import { amber, blueGrey } from '@material-ui/core/colors';
 
 const ReturnCard = ({
   year,
@@ -15,23 +14,23 @@ const ReturnCard = ({
   const classes = useStyles();
 
   return (
-    <Grid item={true} xs={12} sm={6} md={3}>
+    <Grid item={true} xs={6} sm={4} md={3}>
       <Card className={classes.root}>
         <CardContent>
           <div className={classes.gutterBottom}>
-            <Typography style={{ fontSize: 14 }} color="textSecondary" gutterBottom>
+            <Typography style={{ fontSize: 12 }} color="textSecondary" gutterBottom>
               第 {year} 年
           </Typography>
-            <Typography variant="h6" component="h2">
+            <Typography style={{ fontSize: 14 }} variant="h6" component="h2">
               本利和：${revenue}
             </Typography>
-            <Typography variant="body1" component="h2">
+            <Typography style={{ fontSize: 12 }} variant="body1" component="h2">
               (相等於今日嘅：${deflatedRevenue})
           </Typography>
           </div>
 
           <div className={classes.gutterBottom}>
-            <Typography style={{ fontSize: 16 }} color="textSecondary">
+            <Typography style={{ fontSize: 14 }} color="textSecondary">
               全年利潤：${yearlyProfit}
             </Typography>
             <Typography style={{ fontSize: 12 }} color="textSecondary">
@@ -40,10 +39,10 @@ const ReturnCard = ({
           </div>
 
           <div className={classes.gutterBottom}>
-            <Typography variant="body2" component="div">
+            <Typography style={{ fontSize: 12 }} variant="body2" component="div">
               每月投入：${monthlyInput}
             </Typography>
-            <Typography variant="body2" component="div">
+            <Typography style={{ fontSize: 12 }} variant="body2" component="div">
               全年投入：${yearlyInput}
             </Typography>
           </div>
